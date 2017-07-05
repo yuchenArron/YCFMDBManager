0fa9e9e92148de28e5743fffd79eaad12ac2d4d8
yuchenArron <aaron_yuchen@sina.com>
2017-07-05 21:19:22 +0800

    New project

---
+++ YCFMDBManagerDemo/Pods/Target Support Files/Pods-YCFMDBManagerDemoUITests/Pods-YCFMDBManagerDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YCFMDBManagerDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_YCFMDBManagerDemoUITests
+@end
---
+++ YCFMDBManagerDemo/YCFMDBManagerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YCFMDBManagerDemo
+//
+//  Created by mac on 2017/7/5.
+//  Copyright © 2017年 Netposa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
