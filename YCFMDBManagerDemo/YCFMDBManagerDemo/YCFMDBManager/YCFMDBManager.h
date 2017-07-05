4fd378f3a19efbed6353123d9bfff6148c9c9c71
yuchenArron <aaron_yuchen@sina.com>
2017-07-05 21:48:59 +0800

    1

---
+++ YCFMDBManagerDemo/YCFMDBManagerDemo/YCFMDBManager/YCFMDBManager.h
@@ -0,0 +1,33 @@
+//
+//  YCFMDBManager.h
+//  YCFMDBManagerDemo
+//
+//  Created by mac on 2017/7/5.
+//  Copyright © 2017年 Netposa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//默认
+
+@interface YCFMDBManager : NSObject
+
++ (instancetype)shareInstance;
+
+//创建数据库 表
+
+//增
+
+//删
+
+//改
+
+//查
+
+//获取 全部
+
+//删除 全部
+
+
+
+@end
---
+++ YCFMDBManagerDemo/YCFMDBManagerDemo/YCFMDBManager/YCFMDBManager.m
@@ -0,0 +1,32 @@
+
+//
+//  YCFMDBManager.m
+//  YCFMDBManagerDemo
+//
+//  Created by mac on 2017/7/5.
+//  Copyright © 2017年 Netposa. All rights reserved.
+//
+
+#import "YCFMDBManager.h"
+#import <FMDB.h>
+
+static YCFMDBManager *manager = nil;
+
+@interface YCFMDBManager()
+
+@property (nonatomic, strong) FMDatabase *db;
+@property (nonatomic, strong) FMDatabaseQueue *dbQueue;
+
+@end
+
+@implementation YCFMDBManager
+
++ (instancetype)shareInstance{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        manager = [[YCFMDBManager alloc]init];
+    });
+    return manager;
+}
+
+@end
